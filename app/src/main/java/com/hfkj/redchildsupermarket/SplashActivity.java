619b01a4cd5863466f0a680da76dc12033e86e3d
heima14 <805160581@qq.com>
2016-09-04 22:59:24 +0800

    HomePage 第一次提交

--- app/src/main/java/com/hfkj/redchildsupermarket/SplashActivity.java
+++ app/src/main/java/com/hfkj/redchildsupermarket/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.hfkj.redchildsupermarket;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+import com.hfkj.redchildsupermarket.activity.HomePageActivity;
+
+public class SplashActivity extends Activity {
 
 
 
@@ -11,6 +15,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,HomePageActivity.class));
+                SplashActivity.this.finish();
+            }
+        }, 2000);
     }
 }
