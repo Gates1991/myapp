4bb290f1be1806654f3f11c3fd7624cbfb46bd64
栁年 <aaaa>
2016-09-06 18:57:46 +0800

    更新测试

---
+++ app/src/main/java/com/hfkj/redchildsupermarket/bean/SearchRecommandResponse.java
@@ -0,0 +1,38 @@
+package com.hfkj.redchildsupermarket.bean;
+
+import java.util.List;
+
+
+public class SearchRecommandResponse extends BaseResponse {
+    /**
+     * response : searchrecommend
+     * searchKeywords : ["玩具","男士","孕妇"]
+     */
+
+//    private String response;
+    private List<String> searchKeywords;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public List<String> getSearchKeywords() {
+        return searchKeywords;
+    }
+
+    public void setSearchKeywords(List<String> searchKeywords) {
+        this.searchKeywords = searchKeywords;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchRecommandResponse{" +
+                "response='" + response + '\'' +
+                ", searchKeywords=" + searchKeywords +
+                '}';
+    }
+}
