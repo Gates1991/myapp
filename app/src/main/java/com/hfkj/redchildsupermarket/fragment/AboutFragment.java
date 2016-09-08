eac3db537d99dcbd7e417c03f063076a5cad6b8f
栁年 <aaaa>
2016-09-08 19:46:07 +0800

    关于界面完成

--- app/src/main/java/com/hfkj/redchildsupermarket/fragment/AboutFragment.java
+++ app/src/main/java/com/hfkj/redchildsupermarket/fragment/AboutFragment.java
@@ -1,11 +1,13 @@
 package com.hfkj.redchildsupermarket.fragment;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.hfkj.redchildsupermarket.R;
@@ -18,10 +20,11 @@ import com.hfkj.redchildsupermarket.R;
  * @更新时间 $Date$
  * @更新描述 ${TODO}
  */
-public class AboutFragment extends  BaseFragment {
+public class AboutFragment extends  BaseFragment implements View.OnClickListener {
 
 
     private View mView;
+    private TextView mTv;
 
     @Nullable
     @Override
@@ -29,20 +32,48 @@ public class AboutFragment extends  BaseFragment {
         mView = inflater.inflate(R.layout.fragment_about, null);
         mMainActivity.isMainFrament = false;
         initTitleView();
+        initData();
         return mView;
     }
 
     private void initTitleView() {
-        ImageButton mImgbtn_left = (ImageButton) mView.findViewById(R.id.imgbtn_left);
-        mImgbtn_left.setVisibility(View.VISIBLE);
-        TextView mTv_title_left = (TextView) mView.findViewById(R.id.tv_title_left);
-        mTv_title_left.setText("返回");
-        TextView mTv_title_layout = (TextView) mView.findViewById(R.id.tv_title_layout);
-        mTv_title_layout.setText("关于");
+        Button mBt_title_left = (Button) mView.findViewById(R.id.bt_title_left);
+        mBt_title_left.setVisibility(View.VISIBLE);
 
+        TextView mTtv_title = (TextView) mView.findViewById(R.id.tv_title_name);
+        mTtv_title.setText("关于");
+        mTtv_title.setTextSize(24);
+
+        mTv = (TextView) mView.findViewById(R.id.tv);
+        mBt_title_left.setOnClickListener(this);
     }
 
     public void initData() {
+        try {
+            mTv.setText("版本号："+ getVersionName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private String getVersionName() throws Exception{
+        // 获取packagemanager的实例
+        PackageManager packageManager = mContext.getPackageManager();
+        // getPackageName()是你当前类的包名，0代表是获取版本信息
+        PackageInfo packInfo = packageManager.getPackageInfo(mContext.getPackageName(),0);
+        String version = packInfo.versionName;
+        return version;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_title_left:
+        		mMainActivity.popBackStack();
+                break;
+
+            default:
+                break;
+        }
 
     }
 }
