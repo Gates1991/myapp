7701e1168695026d89b531ec27dbf9d1e8e8e1e5
ShayneShao <shaochaowangyi@163.com>
2016-09-08 14:21:36 +0800

    更新

---
+++ app/src/main/java/com/hfkj/redchildsupermarket/fragment/UnsucessIndentFragment.java
@@ -0,0 +1,31 @@
+package com.hfkj.redchildsupermarket.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hfkj.redchildsupermarket.R;
+
+/**
+ * @创建者 Shayne
+ * @创建时间 2016/9/8 9:47
+ * @描述着 ${TODO}
+ * @更新者 $Author$
+ * @更新时间 $Date$
+ * @更新描述 ${TODO}
+ */
+public class UnsucessIndentFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstnceState) {
+        View view = inflater.inflate(R.layout.fragment_unsucessindent, null);
+        return view;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
